5b1aba7c6bc9319f93829e606d7fd33bf50cda1b
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-09-26 18:22:36 +0100

    suprime checkconn

--- functions/checknamedansmap.go
+++ functions/checknamedansmap.go
@@ -1,6 +1,6 @@
 package netino
 
-import "net"
+
 
 func Checknamedansmap ( name string , s *Server) bool {
 	for v := range s.clients {
@@ -9,12 +9,4 @@ func Checknamedansmap ( name string , s *Server) bool {
 		}
 	}
 	return true 
-}
-func Checkconn (con net.Conn, s *Server) bool{
-	for  _, v := range s.clients {
-		if v.cone == con {
-			return true
-		}
-	}
-	return false 
 }
